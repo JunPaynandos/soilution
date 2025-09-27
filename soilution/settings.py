bf3bf18b24f36bd48f13b24bcaa3e42b76d41b27
JunPaynandos <junreypaynandos@gmail.com>
2025-09-27 23:57:14 +0800

    changing to redis channel

--- soilution/settings.py
+++ soilution/settings.py
@@ -174,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer"",
          "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL')],
         },
